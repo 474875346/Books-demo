afe6cde929ef111db1f429d3313773fc69ace7ba
烟汐忆梦 <474875346@qq.com>
2020-05-11 17:30:47 +0800

    controller准备完成

--- src/main/java/com/m/controller/BookController.java
+++ src/main/java/com/m/controller/BookController.java
@@ -1,4 +1,25 @@
 package com.m.controller;
 
-public class BookController {
+import com.m.controller.base.BaseController;
+
+public class BookController extends BaseController {
+    // 展示列表
+    public void toList() {
+
+    }
+
+    // 删除
+    public void del() {
+
+    }
+
+    // 去修改页面
+    public void toUpdate() {
+
+    }
+
+    // 新增或修改
+    public void insertOrUpdate() {
+
+    }
 }
--- src/main/java/com/m/controller/filter/BookFilter.java
+++ src/main/java/com/m/controller/filter/BookFilter.java
@@ -1,6 +1,8 @@
 package com.m.controller.filter;
 
 
+import com.m.controller.BookController;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,12 +17,34 @@ public class BookFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest hreq = (HttpServletRequest)req;
-        HttpServletResponse hresp = (HttpServletResponse)resp;
+        HttpServletRequest hreq = (HttpServletRequest) req;
+        HttpServletResponse hresp = (HttpServletResponse) resp;
         HttpSession session = hreq.getSession();
 
+        // /Books_demo_war/book/create.do
         String uri = hreq.getRequestURI();
         System.out.println(uri);
+        String nameSpace = uri.split("/")[2];
+        String method = uri.split("/")[3];
+
+        if ("book".equals(nameSpace)) {
+            BookController book = new BookController();
+            book.setRequest(hreq);
+            book.setResponse(hresp);
+            book.setSession(session);
+
+            if ("toList.do".equals(method)) {
+                book.toList();
+            } else if ("del.do".equals(method)) {
+                book.del();
+            } else if ("toUpdate.do".equals(method)) {
+                book.toUpdate();
+            } else if ("insertOrUpdate.do".equals(method)) {
+                book.insertOrUpdate();
+            }
+        }
+
+
     }
 
     @Override
